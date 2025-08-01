3790695e5bc3a717da9b47d3eb8794803adefb32
MinaTechStack <minakshikumari207@gmail.com>
2025-08-02 03:24:30 +0530

    sw

--- backend/middleware/authorozation.js
+++ backend/middleware/authorozation.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const authorize = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const token = req.cookies?.admin_token; // ✅ from cookie
 
   if (!token) {
     return res.status(401).json({ message: 'Access token missing' });
