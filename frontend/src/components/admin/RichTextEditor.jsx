881baadfc04445dcde908ee787c366c8d6e6c659
MinaTechStack <minakshikumari207@gmail.com>
2025-08-02 02:01:30 +0530

    nm

--- frontend/src/components/admin/RichTextEditor.jsx
+++ frontend/src/components/admin/RichTextEditor.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useRef, useMemo } from 'react';
-import JoditEditor from 'jodit-react';
+'use client'
+import { useRef, useMemo } from "react";
+import dynamic from "next/dynamic";
+
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 const RichTextEditor = ({ placeholder, value, changeHandler }) => {
     const editor = useRef(null);
 
     const config = useMemo(() => ({
-        readonly: false, // all options from https://xdsoft.net/jodit/docs/,
-        placeholder: placeholder || 'Start typings...'
-    }),
-        [placeholder]
-    );
+        readonly: false,
+        placeholder: placeholder || 'Start typing...',
+    }), [placeholder]);
 
     return (
         <JoditEditor
             ref={editor}
             value={value}
             config={config}
-            tabIndex={1} // tabIndex of textarea
-            onChange={
-                (data) => {
-                    changeHandler(data)
-                }
-            }
+            tabIndex={1}
+            onChange={(data) => changeHandler(data)}
         />
     );
 };
+
 export default RichTextEditor;
