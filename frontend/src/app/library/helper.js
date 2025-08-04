1b6c05b44b6a203800b43481bce6a55360c10968
MinaTechStack <minakshikumari207@gmail.com>
2025-08-04 10:47:42 +0530

    kh

--- frontend/src/app/library/helper.js
+++ frontend/src/app/library/helper.js
@@ -12,7 +12,7 @@ import axios from 'axios';
 
 const axiosApiInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-     withCredentials: true,
+    withCredentials: true,
 });
 
 function createSlug(text) {
@@ -29,11 +29,11 @@ function createSlug(text) {
 
 }
 function getCookies(name) {
-  if (typeof document === 'undefined') return null; // ✅ only run in browser
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
+    if (typeof document === 'undefined') return null; // ✅ only run in browser
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
 }
 
 const notify = (msg, flag) => toast(msg, { type: flag ? "success" : "error" });
