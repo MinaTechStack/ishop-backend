b2ebd404d3a483cae17e428c7f5dfef0103fedf2
MinaTechStack <minakshikumari207@gmail.com>
2025-08-05 18:19:04 +0530

    jh

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -5,8 +5,8 @@ const fileUpload = require('express-fileupload');
 const authorize = require('../middleware/authorozation');
 
 ProductRouter.post("/create", fileUpload({ createParentPath: true }), productController.create);
-ProductRouter.get("/:id?", authorize, productController.read);
-ProductRouter.delete("/delete/:id",authorize, productController.delete);
+ProductRouter.get("/:id?", productController.read);
+ProductRouter.delete("/delete/:id", authorize, productController.delete);
 ProductRouter.patch("/status/:id", productController.status);
 ProductRouter.post("/multiple-images/:id", fileUpload({ createParentPath: true }), productController.multipleImages);
 ProductRouter.put("/update/:id", fileUpload({ createParentPath: true }), productController.update);
