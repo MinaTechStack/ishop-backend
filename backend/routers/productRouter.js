7c4396f8343e6fcb7e9cfd9a8e32396dcda6dff0
MinaTechStack <minakshikumari207@gmail.com>
2025-08-05 18:28:28 +0530

    jh

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -4,10 +4,10 @@ const productController = require('../controllers/productController');
 const fileUpload = require('express-fileupload');
 const authorize = require('../middleware/authorozation');
 
-ProductRouter.post("/create", fileUpload({ createParentPath: true }), productController.create);
+ProductRouter.post("/create",authorize, fileUpload({ createParentPath: true }), productController.create);
 ProductRouter.get("/:id?", productController.read);
 ProductRouter.delete("/delete/:id", authorize, productController.delete);
-ProductRouter.patch("/status/:id", productController.status);
-ProductRouter.post("/multiple-images/:id", fileUpload({ createParentPath: true }), productController.multipleImages);
-ProductRouter.put("/update/:id", fileUpload({ createParentPath: true }), productController.update);
+ProductRouter.patch("/status/:id",authorize, productController.status);
+ProductRouter.post("/multiple-images/:id",authorize, fileUpload({ createParentPath: true }), productController.multipleImages);
+ProductRouter.put("/update/:id",authorize, fileUpload({ createParentPath: true }), productController.update);
 module.exports = ProductRouter;
